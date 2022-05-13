62a47176ef8c5db20ea23e74ab1047689005d9b9
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-13 14:39:41 +0200

    Aggiungere i file di progetto.

---
+++ GetSet.cs
@@ -0,0 +1,23 @@
+﻿using System;/*A partire dalla versione C# 3.0 (.NET 3.5) è stata introdotta la Object Initializer Syntax, un nuovo modo di inizializzare un oggetto di una classe o di una collection Questo nuovo modo ti permette di assegnare valori agli attributi al momento della creazione dell'istanza e senza invocare il costruttore.Da notare la proprietà MATRICOLA dello studente (_myPropertyInternalMatricolaStudente)Di questa variabile una parte la voglio tenere interna all'università (es. i primi 5 bytes)mentre una parte (i secondi 5 bytes) la voglio rendere publica inclusi i tre bytes finali.Esercizio: gestire il fatto che se StudentName non è impostato, allora non potrei impostaretale proprietà. Allora metto i secondi 5 bytes a 0. E la set di StudentName completa l'operazione.*/
+
+namespace EserciziQuartoGiorno{    public class Student    {
+        //Proprietà (privata)
+        private string _myPropertyInternalMatricolaStudente;        public string MyPropertyPublicMatricolaStudente        {            get            {                return _myPropertyInternalMatricolaStudente.Substring(5);            }            set            {                if (Age < 20)                    _myPropertyInternalMatricolaStudente = "00021" + this.StudentName.Substring(0, 5) + value;                else                    _myPropertyInternalMatricolaStudente = "00022" + this.StudentName.Substring(0, 5) + value;            }        }
+
+        //Esempi di Auto-implemented Property
+        // dichiaro le propieta che verranno riempite
+        public int StudentID { get; set; }        public string StudentName { get; set; }        public int Age { get; set; }        public string Address { get; set; }
+
+        
+        public void StampaStudente()
+        {
+            Console.WriteLine("StudenteID {0}", StudentID);
+            Console.WriteLine("nome studente {0}", StudentName);
+            Console.WriteLine("etá studente {0}", Age);
+            Console.WriteLine("indirizzo {0}", Address);
+
+        }            }
+    class EsempioUsoClassStudent    {        //riempio le propieta che ho precedentemente dichiarato        public void EsempioUsoClassStudent_Main()        {
+            //riempio le propieta che ho precedentemente dichiarato creando delle variabili
+            Student std = new Student()            {                StudentID = 1,                StudentName = "Garibardi",                Age = 20,                Address = "New York",                MyPropertyPublicMatricolaStudente = "200"            };            int iStudentID = std.StudentID;            string sAppo = std.MyPropertyPublicMatricolaStudente;
+        }           }}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace EserciziQuartoGiorno
+{ 
+    public class Studente1
+    {
+        private string sMatricolaStudente; // non posso richiamare nel main se non tramite un metodo
+        public string sCognomeStudente; // posso richiamare nel main senza problemi
+
+        //metodo per poter usare la private string (accesso mediato)
+        // questo mi permette di fare dei controlli oppure imporre dei dati specifici
+        public void SetMatricolaStudente(string sIdStudente)
+        {
+            // devo fare qualcosa prima io 
+
+            //Per esempio: connettiti al DB degli studenti e verifica che la matricola esiste giá
+            // Se non esiste la imposti". Se esiste torni un codice d'errore
+            this.sMatricolaStudente = "001" + sIdStudente;
+        }
+        public Studente1()
+        {
+            this.sMatricolaStudente = "";
+        }
+
+        Studente1(string sCognome, string sMatricola)
+        {
+            sCognomeStudente = sCognome;
+            SetMatricolaStudente(sMatricola);
+        }
+
+    }
+    // classse che in questo caso ereditiamo dentro serie
+    public class Contenuto
+    {
+
+        protected string titolo;
+
+        public Contenuto(string titolo)
+        {
+            this.titolo = titolo;
+        }
+
+        public void Riproduci()
+        {
+            Console.WriteLine("Stai guardando " + titolo);
+        }
+    }
+    // classe nuova creata : classe giá esistente da estendere (padre)
+    public class Serie : Contenuto
+    {
+
+        private string[] cast;
+
+        public Serie(string titolo, string[] cast) : base(titolo)
+        {
+            this.cast = cast;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Studente1 MioStudente = new Studente1();
+            MioStudente.sCognomeStudente = "Rossi";
+            MioStudente.SetMatricolaStudente("20321");
+
+            EsempioUsoClassStudent MioEsempio = new EsempioUsoClassStudent();
+            MioEsempio.EsempioUsoClassStudent_Main();
+            */
+            //Student.StampaStudente();
+            
+        }
+
+    }
+}
